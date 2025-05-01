b59f8d3be9c3b44734c7ea5cae7f071f67aff29b
Gregg Abe <greggabe808@gmail.com>
2025-04-30 18:55:30 -1000

    Setup

---
+++ Tally/Tally/TallyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TallyApp.swift
+//  Tally
+//
+//  Created by Gregg Abe on 4/30/25.
+//
+
+import SwiftUI
+
+@main
+struct TallyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
